ce07e6f2dec85e3aa8a4036001e6e88210084ffa
CarryWang <design.carrywang@gmail.com>
2017-03-17 17:32:37 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-
+import Nav from './Nav'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        hello react
+      <div>
+        <Nav/>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 card">
+
+            </div>
+            <div className="col-sm-6 card">
+
+            </div>
+            <div className="col-sm-6 card">
+
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Carry Wang on 2017/3/17.
+ */
+import React, { Component } from 'react';
+
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <div className="myNav">
+
+            </div>
+        );
+    }
+}
+
+
+
