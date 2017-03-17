001f0a37390ce16012927f5d5b083f67ea1231f2
CarryWang <design.carrywang@gmail.com>
2017-03-17 16:20:58 +0800

    update

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        hello react
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
