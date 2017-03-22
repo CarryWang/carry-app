b78044ad09f091837f8c7fe343acb458362b20aa
CarryWang <design.carrywang@gmail.com>
2017-03-22 15:58:12 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,80 @@
 import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import Nav from './Nav';
 import Banner from './Banner';
 import Title from './Title';
 import Contact from './Contact'
 import Photos from './Photos'
+import CardBox from './CardBox'
 import SvgBox from './SvgBox'
-// import img1 from '../images/wallhaven1.jpg'
+import Load from './Load'
+
 
 let obj = {
     title: 'Cases',
     name: 'Some Example Design Patterns'
-}
+
+};
 let obj2 = {
     title: 'Works',
     name: 'Some Works'
-}
+};
 
 
-class App extends Component {
-    render() {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dom: this.loadingDom
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(()=> {
+            this.setState({dom: this.mainDom})
+        }, 4000)
+    }
+
+    mainDom = ()=> {
         return (
-            <div>
+            <ReactCSSTransitionGroup
+                component="div"
+                transitionName="example"
+                transitionAppear={true}
+                transitionAppearTimeout={500}
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={300}>
                 <Nav/>
                 <Banner/>
                 <Title {...obj}/>
-                <div className="container-fluid">
-                    <div className="row">
-                        <a className="col-sm-6 card img1" href="https://carrywang.github.io/demo/3dCard/index.html">
-
-                        </a>
-                        <div className="col-sm-6 card img2">
-
-                        </div>
-                        <div className="col-sm-6 card img3">
-
-                        </div>
-                        <div className="col-sm-6 card img4">
-
-                        </div>
-
-                    </div>
-                </div>
+                <CardBox/>
                 <SvgBox/>
                 <Title {...obj2}/>
                 <Photos/>
                 <Contact/>
-            </div>
-        );
+            </ReactCSSTransitionGroup>
+        )
+    };
+
+    loadingDom = ()=> {
+        return (
+            <ReactCSSTransitionGroup
+                component="div"
+                transitionName="example"
+                transitionAppear={true}
+                transitionAppearTimeout={500}
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={300}>
+                <Load/>
+            </ReactCSSTransitionGroup>
+        )
+    };
+
+    render() {
+        return this.state.dom();
     }
 }
 
-export default App;
+
 
 
---
+++ src/components/CardBox.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Carry Wang on 2017/3/20.
+ */
+import React, {Component} from 'react';
+
+export default class CardBox extends Component{
+    render(){
+        return(
+            <div className="container-fluid">
+                <div className="row">
+                    <a className="col-sm-6 card img1" href="https://carrywang.github.io/demo/3dCard/index.html">
+
+                    </a>
+                    <div className="col-sm-6 card img2">
+
+                    </div>
+                    <div className="col-sm-6 card img3">
+
+                    </div>
+                    <div className="col-sm-6 card img4">
+
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
